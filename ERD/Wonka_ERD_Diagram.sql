4946a5a518ce846525b1ad8f02fb69a7629fb433
jussara08 <jussaraalmeidagaspar3@gmail.com>
2025-12-13 17:08:49 +0100

    Add files via upload
    it has the same name a the ERD but its different , not and ERD

--- ERD/Wonka_ERD_Diagram.sql
+++ ERD/Wonka_ERD_Diagram.sql
@@ -51,3 +51,189 @@ ALTER TABLE `sales` ADD FOREIGN KEY (`product_id`) REFERENCES `products` (`produ
 ALTER TABLE `sales` ADD FOREIGN KEY (`factory_id`) REFERENCES `factories` (`factory_id`);
 
 ALTER TABLE `sales` ADD FOREIGN KEY (`location_id`) REFERENCES `locations` (`location_id`);
+
+USE wonka_choc_factory_clean;
+-- CUSTOMERS
+INSERT INTO customers (customer_id, country_region, city, state_province, postal_code)
+SELECT DISTINCT
+    `Customer ID`,
+    `Country/Region`,
+    City,
+    `State/Province`,
+    `Postal Code`
+FROM wonka_choc_factory_clean;
+
+-- FACTORIES
+
+INSERT INTO factories (factory_name, latitude, longitude)
+SELECT DISTINCT
+    Factory,
+    Latitude,
+    Longitude
+FROM wonka_choc_factory_clean;
+INSERT INTO factories (factory_name, latitude, longitude)
+SELECT DISTINCT
+    Factory,
+    Latitude,
+    Longitude
+FROM wonka_choc_factory_clean;
+
+SELECT DISTINCT Latitude, Longitude
+FROM wonka_choc_factory_clean
+LIMIT 10;
+
+-- LOCATIONS
+
+INSERT INTO locations (city, state_province, region, country_region)
+SELECT DISTINCT
+    City,
+    `State/Province`,
+    Region,
+    `Country/Region`
+FROM wonka_choc_factory_clean;
+
+-- PRODUCTS
+
+INSERT INTO products (product_name, division, region, cost)
+SELECT DISTINCT
+    `Product Name`,
+    Division,
+    Region,
+    Cost
+FROM wonka_choc_factory_clean;
+-- changing  cost into  decimals
+ALTER TABLE products
+MODIFY cost DECIMAL(10,2);
+
+-- SALES
+
+
+INSERT INTO sales (
+    order_date, 
+    ship_date, 
+    ship_mode, 
+    customer_id, 
+    product_id, 
+    factory_id, 
+    location_id,
+    sales_amount, 
+    units, 
+    gross_profit, 
+    cost
+)
+SELECT
+    w.`Order Date`,
+    w.`Ship Date`,
+    w.`Ship Mode`,
+    c.customer_id,
+    p.product_id,
+    f.factory_id,
+    l.location_id,
+
+    /* CLEAN SALES */
+    CASE 
+        WHEN w.Sales REGEXP '^[0-9]+(\.[0-9]+)?$'
+        THEN CAST(w.Sales AS DECIMAL(10,2))
+        ELSE NULL
+    END AS sales_amount,
+
+    w.Units,
+
+    /* CLEAN GROSS PROFIT */
+    CASE 
+        WHEN w.`Gross Profit` REGEXP '^[0-9]+(\.[0-9]+)?$'
+        THEN CAST(w.`Gross Profit` AS DECIMAL(10,2))
+        ELSE NULL
+    END AS gross_profit,
+
+    /* CLEAN COST */
+    CASE 
+        WHEN w.Cost REGEXP '^[0-9]+(\.[0-9]+)?$'
+        THEN CAST(w.Cost AS DECIMAL(10,2))
+        ELSE NULL
+    END AS cost
+
+FROM wonka_choc_factory_clean w
+JOIN customers c ON w.`Customer ID` = c.customer_id
+JOIN products p ON w.`Product Name` = p.product_name
+JOIN factories f ON w.Factory = f.factory_name
+JOIN locations l 
+   ON w.City = l.city
+  AND w.`State/Province` = l.state_province
+  AND w.Region = l.region
+  AND w.`Country/Region` = l.country_region;
+
+
+SELECT Sales
+FROM wonka_choc_factory_clean
+WHERE Sales NOT REGEXP '^[0-9.+-]+$'
+LIMIT 100;
+
+SHOW COLUMNS FROM sales;
+
+ALTER TABLE sales
+MODIFY COLUMN sales_amount DECIMAL(15,2),
+MODIFY COLUMN gross_profit DECIMAL(15,2),
+MODIFY COLUMN cost DECIMAL(15,2);
+
+TRUNCATE TABLE sales;
+
+INSERT INTO sales (
+    order_date, 
+    ship_date, 
+    ship_mode, 
+    customer_id, 
+    product_id, 
+    factory_id, 
+    location_id,
+    sales_amount, 
+    units, 
+    gross_profit, 
+    cost
+)
+SELECT
+    w.`Order Date`,
+    w.`Ship Date`,
+    w.`Ship Mode`,
+    c.customer_id,
+    p.product_id,
+    f.factory_id,
+    l.location_id,
+
+    /* CLEAN SALES */
+    CASE 
+        WHEN w.Sales REGEXP '^[0-9]+(\.[0-9]+)?$'
+        THEN CAST(w.Sales AS DECIMAL(10,2))
+        ELSE NULL
+    END AS sales_amount,
+
+    w.Units,
+
+    /* CLEAN GROSS PROFIT */
+    CASE 
+        WHEN w.`Gross Profit` REGEXP '^[0-9]+(\.[0-9]+)?$'
+        THEN CAST(w.`Gross Profit` AS DECIMAL(10,2))
+        ELSE NULL
+    END AS gross_profit,
+
+    /* CLEAN COST */
+    CASE 
+        WHEN w.Cost REGEXP '^[0-9]+(\.[0-9]+)?$'
+        THEN CAST(w.Cost AS DECIMAL(10,2))
+        ELSE NULL
+    END AS cost
+
+FROM wonka_choc_factory_clean w
+JOIN customers c ON w.`Customer ID` = c.customer_id
+JOIN products p ON w.`Product Name` = p.product_name
+JOIN factories f ON w.Factory = f.factory_name
+JOIN locations l 
+   ON w.City = l.city
+  AND w.`State/Province` = l.state_province
+  AND w.Region = l.region
+  AND w.`Country/Region` = l.country_region;
+
+
+
+
+
